57689f6a372db766917a27903a29a89801384810
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2022-01-22 22:28:42 -0600

    added getValues function and validation

--- js/site.js
+++ js/site.js
@@ -1,12 +1,29 @@
 //display message from the user:
 function getValues() {
-    let userString = document.getElementById("revString").value;
-    let RevString = reverseAString(userString);
-    //display Reverse String
-    displayData(revString);
+    let tPrincipal = document.getElementById("loanAmt").value;
+    let numPayments = document.getElementById("numPayments").value;
+    let interestRate = document.getElementById("interestRate").value;
+    //VAIDATION:
+    //Parse for numbers(inputs)
+    tPrincipal = parseInt(tPrincipal);
+    numPayments = parseInt(numPayments);
+    interestRate = parseInt(interestRate);
+    //convert strings to integers
+    if (Number.isInteger(tPrincipal) && Number.isInteger(numPayments) && Number.isInteger(interestRate)) {
+        let lcArray = loanCalc(tPrincipal, numPayments, interestRate)
+        //Call displayData & write values to the DOM;
+        displayData(lcArray);
+    } else {
+        alert("Try again. Please enter only numbers into the loan calculator")
+    }
+
+    //display Right Col 2 data
+
+    displayData();
 }
 //LOGIC: (old generate )
-function reverseAString(userString) {
+//Generate an array of payment objects
+/*function reverseAString(userString) {
     let revString = "",
         //decrimenting for loop
         for (let i = userString.length - 1; i >= 0; i--) {
@@ -15,12 +32,11 @@ function reverseAString(userString) {
             return revString;
 
         }
-}
-//Display back to DOM
-function displayData(revString) {
-    document.getElementById("msg").innerHTML = `Your string is: ${revString}`;
+}*/
 
-    document.getElementById("alert").classList.remove('invisible');
-    document.getElementById("alert").classList.remove("d-none");
+//Display back to DOM
+function displayData() {
+    document.getElementById("tPrincipal").innerHTML = `$ ${tPrincipal}`;
+
 
 }
