05d23a218eb2c9932be8c7d456f55558d2dc8911
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2022-01-23 20:14:37 -0600

    built generate payments function

--- js/site.js
+++ js/site.js
@@ -1,42 +1,73 @@
-//display message from the user:
+//get message from the user:
 function getValues() {
     let tPrincipal = document.getElementById("loanAmt").value;
     let numPayments = document.getElementById("numPayments").value;
     let interestRate = document.getElementById("interestRate").value;
+    //convert interestRate to %
+    let term = interestRate / 100;
     //VAIDATION:
     //Parse for numbers(inputs)
     tPrincipal = parseInt(tPrincipal);
     numPayments = parseInt(numPayments);
-    interestRate = parseInt(interestRate);
+    term = parseFloat(term);
     //convert strings to integers
-    if (Number.isInteger(tPrincipal) && Number.isInteger(numPayments) && Number.isInteger(interestRate)) {
-        let lcArray = loanCalc(tPrincipal, numPayments, interestRate)
-        //Call displayData & write values to the DOM;
-        displayData(lcArray);
-    } else {
-        alert("Try again. Please enter only numbers into the loan calculator")
-    }
-
-    //display Right Col 2 data
-
-    displayData();
-}
-//LOGIC: (old generate )
-//Generate an array of payment objects
-/*function reverseAString(userString) {
-    let revString = "",
-        //decrimenting for loop
-        for (let i = userString.length - 1; i >= 0; i--) {
-            revString += userString[index];
-
-            return revString;
-
+    if (Number.isInteger(tPrincipal) && Number.isInteger(numPayments) && Number.isInteger(term) {
+            let lcArray = generatePayments(tPrincipal, numPayments, term)
+            //Call displayData & write values to the DOM;
+            displayData(lcArray);
+        } else {
+            alert("Try again. Please enter only numbers into the loan calculator.")
         }
-}*/
+
+        //display Right Col 2 data
+
+        displayData();
+    }
+    //LOGIC: 
+    //Generate an array of payment objects:
+    function generatePayments(tPrincipal, numPayments, term) {
+
+        let paymentsArray = [];
+        let paymentobj = {};
+
+        //generating months to the object
+        let months = [];
+        let monthPayment = [];
+        for (let i = 1; i <= numPayments; i++) {
+            months.push[i];
+            paymentobj.push(months);
+        }
+        paymentsArray.push(paymentobj);
+
+        //COL 2 (RIGHT) DATA
+        //Calculate Monthly Payments
+        monthPayment = tPrincipal * (term / 1200) / Math.pow(1 - (1 + term / 1200), months);
+        paymentobj.push(monthPayment);
+        paymentsArray.push(paymentobj);
+
+        //Calculate Total interest
+        let tInterest = tPrincipal * term * months);
+    paymentobj.push(tInterest);
+    paymentsArray.push(paymentobj);
+
+    //Calculate Total loan cost
+    let tCost = tPrincipal + tInterest;
+
+    /* for (let i = 1; i <= paymentsArray.length; i++) {
+
+     }*/
+
+
+
+
+}
+
+
 
 //Display back to DOM
 function displayData() {
-    document.getElementById("tPrincipal").innerHTML = `$ ${tPrincipal}`;
-
-
+    let displayPayment = document.getElementById("displayPayment").innerHTML = `&#36; ${monthPayment}`;
+    let displayPrincipal = document.getElementById("tPrincipal").innerHTML = `&#36; ${tPrincipal}`;
+    let displayTotalInterest = document.getElementById("tInterest").innerHTML = `&#36;${tInterest}`;
+    let displayTotalCost = document.getElementById("tCost").innerHTML = `&#36; ${tCost}`;
 }
