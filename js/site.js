254d198c20b16f7fd9445df50779f33a51660060
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2022-01-23 20:19:43 -0600

    fixing minor err in js

--- js/site.js
+++ js/site.js
@@ -11,42 +11,42 @@ function getValues() {
     numPayments = parseInt(numPayments);
     term = parseFloat(term);
     //convert strings to integers
-    if (Number.isInteger(tPrincipal) && Number.isInteger(numPayments) && Number.isInteger(term) {
-            let lcArray = generatePayments(tPrincipal, numPayments, term)
-            //Call displayData & write values to the DOM;
-            displayData(lcArray);
-        } else {
-            alert("Try again. Please enter only numbers into the loan calculator.")
-        }
-
-        //display Right Col 2 data
-
-        displayData();
+    if (Number.isInteger(tPrincipal) && Number.isInteger(numPayments) && Number.isInteger(term)) {
+        let lcArray = generatePayments(tPrincipal, numPayments, term)
+        //Call displayData & write values to the DOM;
+        displayData(lcArray);
+    } else {
+        alert("Try again. Please enter only numbers into the loan calculator.")
     }
-    //LOGIC: 
-    //Generate an array of payment objects:
-    function generatePayments(tPrincipal, numPayments, term) {
 
-        let paymentsArray = [];
-        let paymentobj = {};
+    //display Right Col 2 data
 
-        //generating months to the object
-        let months = [];
-        let monthPayment = [];
-        for (let i = 1; i <= numPayments; i++) {
-            months.push[i];
-            paymentobj.push(months);
-        }
-        paymentsArray.push(paymentobj);
+    displayData();
+}
+//LOGIC: 
+//Generate an array of payment objects:
+function generatePayments(tPrincipal, numPayments, term) {
 
-        //COL 2 (RIGHT) DATA
-        //Calculate Monthly Payments
-        monthPayment = tPrincipal * (term / 1200) / Math.pow(1 - (1 + term / 1200), months);
-        paymentobj.push(monthPayment);
-        paymentsArray.push(paymentobj);
+    let paymentsArray = [];
+    let paymentobj = {};
 
-        //Calculate Total interest
-        let tInterest = tPrincipal * term * months);
+    //generating months to the object
+    let months = [];
+    let monthPayment = [];
+    for (let i = 1; i <= numPayments; i++) {
+        months.push[i];
+        paymentobj.push(months);
+    }
+    paymentsArray.push(paymentobj);
+
+    //COL 2 (RIGHT) DATA
+    //Calculate Monthly Payments
+    monthPayment = tPrincipal * (term / 1200) / Math.pow(1 - (1 + term / 1200), months);
+    paymentobj.push(monthPayment);
+    paymentsArray.push(paymentobj);
+
+    //Calculate Total interest
+    let tInterest = (tPrincipal * term * months);
     paymentobj.push(tInterest);
     paymentsArray.push(paymentobj);
 
